c66dd506a2ff8de4a0a5156bd0a1e5993021749d
Shubhranshu Shekhar Das <134123175+dragINDIANA8@users.noreply.github.com>
2024-07-20 01:12:33 +0530

    Add files via upload

---
+++ Linear_Search.java
@@ -0,0 +1,39 @@
+public class Linear_Search {
+    public static void main(String[] args) {
+        // linear search algorithm
+        /*
+        int[] arr = {1,2,3,4,5,6,10};
+        int num = 56;
+        boolean isInArray = false;
+        for(int i=0; i<arr.length; i++){
+            if (arr[i]==num){
+                isInArray = true;
+            }
+        }
+        if (isInArray){
+            System.out.println("the element is present in the array");
+
+        }
+        else {
+            System.out.println("the element is not present in the array");
+        }
+
+         */
+
+        // linear search algorithm of string
+        String []arr = {"shubh","rohit","abhishek","ashish"};
+        String name = "ajay";
+        boolean isInArray = false;
+        for (int i =0; i<arr.length; i++){
+            if (arr[i].equals(name)){
+                isInArray=true;
+            }
+        }
+        if (isInArray){
+            System.out.println("The element is present in the array");
+        }
+        else{
+            System.out.println("the element is not present in the array");
+        }
+    }
+}
