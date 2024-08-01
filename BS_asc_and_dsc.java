a193e16f34d4c7128e525b332a27e5dcc468a307
Shubhranshu Shekhar Das <134123175+dragINDIANA8@users.noreply.github.com>
2024-08-01 21:17:28 +0530

    Update BS_asc_and_dsc.java

--- BS_asc_and_dsc.java
+++ BS_asc_and_dsc.java
@@ -1,6 +1,6 @@
 public class BS_asc_and_dsc {
     public static void main(String[] args) {
-        int[]arr={8,7,6,5,4,3,2,1};      //  descending order array
+        int[]arr={8,7,6,5,4,3,2,1};      // descending order array
         // int[]arr = (1,2,3,4,5,6,7,8);   // ascending order array
         int target = 3;
         int li = 0;
